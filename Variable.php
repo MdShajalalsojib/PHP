801e17e3e4f47d4d54a4dccce6e8513aa15fe4e3
MdShajalalsojib <shahjalalsojib088@gmail.com>
2025-11-30 23:37:12 +0600

    Variable in  PHP

--- Variable.php
+++ Variable.php
@@ -4,4 +4,17 @@ $age = 23 ;
 $height = 5.9 ;
 $isStudent = true;
   
-echo $age , $name , $height , $isStudent;
+echo $age , $name , $height , $isStudent ;
+echo "My Name is   $name";
+
+?>
+
+<?php
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+?>
+
